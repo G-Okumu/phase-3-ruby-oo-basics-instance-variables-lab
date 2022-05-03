9adb5dfca9cc18ded94584fa834abb3412b4a177
George-Okumu <gokumu12@gmail.com>
2022-05-03 20:27:29 +0300

    assignment complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+    # setter method/ sets the value of instance
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    # getter method/ retrieves the value of instance
+    def name
+        @this_dogs_name
+    end
 end
